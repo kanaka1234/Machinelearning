b601685dae80279da8145cf926de83785ccd31fe
kanaka1234 <gtkanaka6@gmail.com>
2022-10-06 10:53:16 +0530

    added

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 def index():
-    return " startting"
+    return "My first project got deployed"
 if __name__=="__main__":
     app.run(debug=True)
