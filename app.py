879d63ca0d1433369b8abded04850e9fa36034ad
kanaka1234 <gtkanaka6@gmail.com>
2022-10-19 16:07:19 +0530

    addnew

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import logging
+from housing.logger import logging
 from flask import Flask
 app=Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 def index():
+    logging.info("hello")
     return "My first project got deployed"
 if __name__=="__main__":
     app.run(debug=True)
